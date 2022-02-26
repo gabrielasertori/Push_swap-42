875fc6a72b42eebf1b404f4dc11eef8cfdd8b689
gabrielasertori <gabriela@sertori.com.br>
2022-02-26 16:36:33 +0100

    init header logic

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,27 @@
 /*   By: gcosta-d <gcosta-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 16:23:58 by gcosta-d          #+#    #+#             */
-/*   Updated: 2022/02/26 16:24:29 by gcosta-d         ###   ########.fr       */
+/*   Updated: 2022/02/26 16:35:08 by gcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* ==== rules functions ==== */
+void	pa(void);
+void	pb(void);
+void	ra(void);
+void	rb(void);
+void	rr(void);
+void	sa(void);
+void	sb(void);
+void	ss(void);
+void	rra(void);
+void	rrb(void);
+void	rrr(void);
+
+#endif
